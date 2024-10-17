36e9152765b94c3434e9c1f865aa0ecbef0f440f
A1ex4ndr0 <a1ex.n.brb@gmail.com>
2024-10-17 23:35:50 +0300

    1_2 17.10.24

---
+++ 1_2.py
@@ -0,0 +1,5 @@
+completed_tasks = 12
+hours_spent = 1.5
+course = 'Python'
+Time_for_one_task = hours_spent / completed_tasks
+print('Курс:',course,',всего задач:',completed_tasks,',затрачено часов:',hours_spent,',среднее время выполнения',Time_for_one_task,'часа.')
